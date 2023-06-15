ac368b0113c3e45873b8ee793dc5b04bcd0525be
t0931791533 <134581657+t0931791533@users.noreply.github.com>
2023-06-15 16:32:55 +0800

    index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,13 @@
 <template>
   <main>
     <div class="intro">
-      <h1>Nuxt Toolbox</h1>
-      <p>
-        Hi 👋! This template gives you a
-        <a href="https://nuxtjs.org/">Nuxt</a> app with the scaffolding for
-        <a href="https://www.netlify.com/products/functions/"
-          >Netlify Functions</a
-        >, <a href="https://www.netlify.com/products/forms/">Forms</a>, and
-        <a href="https://docs.netlify.com/routing/redirects/">Redirects</a>. Our
-        aim was to give you the code you would need to hit the ground running
-        with a few fun features.
-      </p>
-
-      <p>
-        You can find the code for this project on GitHub at
-        <a href="https://github.com/netlify-templates/nuxt-toolbox"
-          >https://github.com/netlify-templates/nuxt-toolbox</a
-        >! Happy coding!
+      <h1>1</h1>
+      <p>歡迎來到我首頁
+1
+      </p>提供相關信件
       </p>
     </div>
-    <FeedbackForm />
+    <FeedbackForm 
     <JokeBlock />
   </main>
 </template>
